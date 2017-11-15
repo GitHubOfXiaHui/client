6ea3742185782fadb999bbf13294b0ee463f4aee
xiahui <515065283@qq.com>
2017-11-15 17:11:46 +0800

    添加测试

--- src/test/java/com/bupt/client/CipherUtilsTest.java
+++ src/test/java/com/bupt/client/CipherUtilsTest.java
@@ -17,8 +17,8 @@ public class CipherUtilsTest {
 
 	@Autowired
 	private CipherUtils cipher;
-	
-	@Test
+
+	//@Test
 	public void postTest() throws Exception {
 		Post post = new Post();
 		post.setTitle("whatever");
@@ -27,6 +27,21 @@ public class CipherUtilsTest {
 		Post post2 = cipher.decrypt(Post.class, secret, KeyEnum.POST);
 		System.out.println("Post: " + post);
 		System.out.println("Post2: " + post2);
-}
+	}
 	
+	//@Test
+	public void digestTest() throws Exception {
+		String str = "波风更夜";
+		System.out.println("*******" + cipher.digest(str) + "*******");
+	}
+	
+	@Test
+	public void encryptTest() throws Exception {
+		Post post = new Post();
+		post.setTitle("个人信息");
+		post.setContent("用户名：夏辉，昵称：波风更夜。");
+		String secret = cipher.encrypt(post, KeyEnum.POST);
+		System.out.println("*******" + secret + "*******");
+	}
+
 }
